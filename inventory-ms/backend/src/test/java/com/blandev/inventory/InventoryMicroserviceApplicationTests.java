0b8ef3395b86db5e44a67ce65da10c4e11f781a5
Jacobo Blandón Castro <jacoboblandon94@gmail.com>
2025-03-24 21:50:32 -0500

    feat: initialize inventory microservice backend
    This commit sets up the initial structure for the inventory microservice
    backend. It includes the following:
    
    - Added `.gitattributes` for consistent line endings.
    - Configured `pom.xml` with Spring Boot dependencies, MariaDB, and
    Lombok.
    - Created the main application class `InventoryMicroserviceApplication`.
    - Added `application.properties` for database and server configuration.
    - Included a basic test class `InventoryMicroserviceApplicationTests`.
    
    This setup provides a foundation for managing product inventory with
    CRUD
    operations, MariaDB integration, and Docker support.

---
+++ inventory-ms/backend/src/test/java/com/blandev/inventory/InventoryMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blandev.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
